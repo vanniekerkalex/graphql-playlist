c0262604a77f2fe2ed9739cedccfde789f1de5d4
vanniekerkalex <vanniekerk.alex@gmail.com>
2018-11-28 17:40:54 +0200

    Adding separate graphql query files

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getAuthorsQuery } from "../queries/queries";
+
+class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+  displayAuthors() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <option>Loading authors...</option>;
+    } else {
+      return data.authors.map(author => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
+      });
+    }
+  }
+  submitForm(e) {}
+  render() {
+    return (
+      <form id="add-book" onSubmit={this.submitForm.bind(this)}>
+        <div className="field">
+          <label>Book name:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+        </div>
+
+        <div className="field">
+          <label>Genre:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ genre: e.target.value })}
+          />
+        </div>
+
+        <div className="field">
+          <label>Author:</label>
+          <select onChange={e => this.setState({ authorId: e.target.value })}>
+            <option>Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
+    );
+  }
+}
+
+export default graphql(getAuthorsQuery)(AddBook);
